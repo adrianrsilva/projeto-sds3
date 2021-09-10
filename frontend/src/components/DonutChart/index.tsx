dff89df6d2a791d86eafd98bb7b0996f9bf7f909
Adrian Silva <adrianrsilva@outlook.com>
2021-09-10 14:13:17 -0300

    Barchart integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -42,11 +42,6 @@ const DonutChart = () => {
     //      console.log(response.data);
     //  })
 
-    //const mockData = {
-    //  series: [477138, 499928, 444867, 220426, 473088],
-    //labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-    //}
-
     const options = {
         legend: {
             show: true
